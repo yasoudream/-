847cea4a8935974332d2894fac42290ffc0c5159
yasoudream <634358229@qq.com>
2020-03-13 22:48:42 +0800

    branch test

---
+++ test/test.cpp
@@ -0,0 +1,17 @@
+﻿// test.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+	printf_s("wtf?\n");
+	for (int i = 0; i < argc; i++)
+	{
+		std::cout << argv[i] << std::endl;
+	}
+	printf_s("wtf2?\n");
+	getchar();
+	return 0;
+}
